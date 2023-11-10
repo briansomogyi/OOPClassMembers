f9feb9e9ed30bac15019e9580ff5ec17369c1b2c
briansomogyi <brian.somogyi@emanuel.ro>
2023-11-10 09:53:41 +0200

    Done some changes

---
+++ OOPClassMembers/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPClassMembers
+{
+    internal class Person
+    {
+        private string _firstName = "John";
+        private string _lastName = "Doe";
+        // Readonly field can be modified from the initialization expression
+        private readonly string _cnp = "1234";
+        public static int PopulationCount = 0;
+        public readonly int MaxLifespanYears = InitMaxLifespan();
+        // This is a constructor
+        public Person(string cnp)
+        {
+            // Readonly field can be modified from the constructor
+            _cnp = cnp;
+        }
+        public void PrintPerson()
+        {
+            Console.WriteLine($"{_firstName} {_lastName}");
+        }
+        public static void PrintPopulationCount()
+        {
+            Console.WriteLine(PopulationCount);
+        }
+        public static int InitMaxLifespan()
+        {
+            Console.Write("Max Lifespan? ");
+            bool ok = int.TryParse(Console.ReadLine(), out int result);
+            if (ok)
+            {
+                return result;
+            }
+            return 0;
+        }
+    }
+}
---
+++ OOPClassMembers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPClassMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("2344");
+            // Console.WriteLine(p._firstName);
+            // Console.WriteLine(p._lastName);
+            Console.WriteLine(p.MaxLifespanYears);
+            // Console.WriteLine(p._cnp);
+            Person.PopulationCount = 1;
+        }
+    }
+}
