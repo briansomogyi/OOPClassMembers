95bb09057776b5f7d7bf2e47835c700dcdc2442b
briansomogyi <brian.somogyi@emanuel.ro>
2023-11-13 09:38:09 +0200

    Added support for array

--- OOPClassMembers/Person.cs
+++ OOPClassMembers/Person.cs
@@ -8,16 +8,11 @@ namespace OOPClassMembers
 {
     internal class Person
     {
-        private string _firstName = "John";
-        private string _lastName = "Doe";
         private string _name;
 
-        private static int _populationCount = 0;
-        
         // Readonly field can be modified from the initialization expression
-        private readonly string _cnp = "1234";
         public readonly int MaxLifespanYears = InitMaxLifespan();
-        
+
         // aceasta este o proprietate
         public string Name
         {
@@ -43,36 +38,24 @@ namespace OOPClassMembers
             }
         }
 
-        public string FirstName
-        {
-            get { return this._firstName; }
-            set { this._firstName = value; }
-        }
+        public string FirstName { get; set; } = string.Empty;
 
-        public string LastName
-        {
-            get { return this._lastName; }
-            set { this._lastName = value; }
-        }
+        public string LastName { get; set; } = string.Empty;
 
-        public static int PopulationCount
-        {
-            get { return Person._populationCount; }
-            set { Person._populationCount = value; }
-        }
+        public static int PopulationCount { get; private set; } = 0;
 
         public string Cnp
         {
-            get { return this._cnp; }
+            get;
         }
 
         // This is a constructor
         public Person(string cnp)
         {
             // Readonly field can be modified from the constructor
-            this._cnp = cnp;
+            Cnp = cnp;
             // Each time a person is constructed the population count is incremented by 1
-            Person._populationCount++;
+            Person.PopulationCount++;
         }
 
         public void SayHello()
@@ -84,23 +67,19 @@ namespace OOPClassMembers
 
         public void PrintPerson()
         {
-            Console.WriteLine($"{_firstName} {_lastName}");
+            Console.WriteLine($"{FirstName} {LastName} - CNP: {Cnp}");
         }
 
         public static void PrintPopulationCount()
         {
-            Console.WriteLine(Person._populationCount);
+            Console.WriteLine(Person.PopulationCount);
         }
 
         public static int InitMaxLifespan()
         {
-            Console.Write("Max Lifespan? ");
-            bool ok = int.TryParse(Console.ReadLine(), out int result);
-            if (ok)
-            {
-                return result;
-            }
-            return 0;
+            return 125;
+            // Console.Write("Max Lifespan? ");
+            // return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
--- OOPClassMembers/Program.cs
+++ OOPClassMembers/Program.cs
@@ -4,19 +4,64 @@
     {
         public static void Main()
         {
-            Person p = new Person("2234");
-            Console.Write("What's your name? "); p.Name = Console.ReadLine();
-            p.SayHello();
-            
-            int maxLifespan = Person.InitMaxLifespan();
-            p.FirstName = "Brian";
-            p.LastName = "Somogyi";
-            // p.Cnp="2344";
-            
-            Console.WriteLine(Person.PopulationCount);
-            Console.WriteLine(p.FirstName);
-            Console.WriteLine(p.LastName);
-            Console.WriteLine(p.Cnp);
+            int numberOfPersons = ReadNumber("Number of Persons: ", 3);
+            Person[] arrayOfPersons=new Person[numberOfPersons];
+            for (int i = 0; i < numberOfPersons; i++)
+            {
+                Console.WriteLine($"Reading person at index {i}: ");
+                
+                Console.Write("FirstName: ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("LastName: ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("CNP: ");
+                string cnp = Console.ReadLine();
+
+                Person p = new Person(cnp);
+                p.FirstName = firstName;
+                p.LastName = lastName;
+
+                arrayOfPersons[i] = p;
+            }
+
+            Console.WriteLine("Printing persons: ");
+            for (int i = 0;i< arrayOfPersons.Length;i++) 
+            {
+                // arrayOfPersons[i].PrintPerson();
+                Person p = arrayOfPersons[i];
+                Console.WriteLine($"{p.FirstName} {p.LastName} - CNP: {p.Cnp}");
+            }
+
+            Console.WriteLine($"Total population count: {Person.PopulationCount}");
+        }
+
+        public static int ReadNumber(string label, int maxAttempts)
+        {
+            int attempts = 1;
+            do
+            {
+                Console.Write(label);
+                string input = Console.ReadLine();
+                bool isValidNumber = int.TryParse(input, out int result);
+
+                if (isValidNumber)
+                {
+                    return result;
+                }
+
+                if (attempts < maxAttempts)
+                {
+                    Console.WriteLine($"Value '{input}' is not a valid number, please try again.");
+                }
+
+                attempts++;
+            }
+            while (attempts <= maxAttempts);
+
+            Console.WriteLine($"No valid numeric input, continuing with 0 as default value.");
+            return 0;
         }
     }
 }
